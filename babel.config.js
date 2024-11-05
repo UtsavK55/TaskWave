fbad9f3cb590c2b66e7639181133b1d87a2932c2
UtsavK55 <utsav@gkmit.co>
2024-11-05 16:17:23 +0530

    fix: necessary package changes

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@config': './src/config',
+          '@helpers': './src/helpers',
+          '@network': './src/network',
+          '@theme': './src/theme',
+          '@assets': './src/assets',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@contexts': './src/contexts',
+          '@navigation': './src/navigation',
+          '@storage': './src/storage',
+          '@types': './src/types',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv', // Add this line for environment variables
+      {
+        moduleName: '@env', // This is the module name you'll use to import
+        path: '.env', // Path to your .env file
+      },
+    ],
+  ],
 };
